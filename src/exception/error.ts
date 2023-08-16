dcbc2e99d40365e21a47d547970e2e3874901120
Judahh <judahholanda7@gmail.com>
2023-08-16 11:00:13 -0300

    improve constructor

--- src/exception/error.ts
+++ src/exception/error.ts
@@ -1,8 +1,14 @@
 class SmartError extends Error {
   isWarning = false;
   static count = 0;
-  constructor(message: string, options?: ErrorOptions) {
-    super(message, options);
+  constructor(message: string | Error | unknown, options?: ErrorOptions) {
+    super(
+      typeof message === 'string' ? message : (message as Error).message,
+      options
+    );
+    if ((message as Error).stack) {
+      this.stack = (message as Error).stack;
+    }
     SmartError.count++;
   }
 }
--- src/exception/warning.ts
+++ src/exception/warning.ts
@@ -1,8 +1,14 @@
 class Warning extends Error {
   isWarning = true;
   static count = 0;
-  constructor(message: string, options?: ErrorOptions) {
-    super(message, options);
+  constructor(message: string | Error | unknown, options?: ErrorOptions) {
+    super(
+      typeof message === 'string' ? message : (message as Error).message,
+      options
+    );
+    if ((message as Error).stack) {
+      this.stack = (message as Error).stack;
+    }
     Warning.count++;
   }
 }
